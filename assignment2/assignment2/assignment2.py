30686d0696e6824986dc828a2c5411e31af60d0a
Edisc <long.voitpro2k@hcmut.edu.vn>
2020-12-03 15:21:17 +0700

    Add more command

--- assignment2/assignment2/assignment2.py
+++ assignment2/assignment2/assignment2.py
@@ -3,14 +3,16 @@ from functools import reduce
 from random import randint,choice,random
 from copy import deepcopy as dp
 from time import time
+import sys
 
 def assign(file_input, file_output):
     depot ,amount,shipperNum ,packages,weightMatrix,offset =[None]*4 +[{}] +[0]
     # nonlocal depot ,amount,shipperNum ,packages, Map
-    WEIGHT = 4
+    '''Gán chỉ sổ để truy xuất thể tích, khối lượng gói hàng dễ dàng'''
+    WEIGHT  = 4
     VOLUMNE = 3
 
-    def readInput():
+    def ReadInput():
         nonlocal depot ,amount,shipperNum ,packages
         file    = open(file_input,"r")
         res     = []
@@ -24,13 +26,17 @@ def assign(file_input, file_output):
         amount,shipperNum   = res[1]
         packages            = [[i] + ele for i,ele in enumerate(res[2:])]
                       
-    def calProfit(begin,package):
+    def CalProfit(begin,package):
         return (
             5  +package[VOLUMNE] + package[WEIGHT]*2
             - (((begin[1]-package[1])**2 +(begin[2]-package[2])**2 )**(1/2))*1/2 
         ) - (10 if begin[0] == -1 else 0)
 
-    def mapNode():
+    def MapNode():
+        '''
+        Tạo ma trận để lưu vị trí các gói hàng và 
+        chi phí từng gói hàng đến các gói hàng còn lại: weightMatrix[i][j]
+        '''
         nonlocal depot ,amount,shipperNum ,packages, weightMatrix
         weightMatrix = [[0]*(amount+1) for i in range(amount + 1)]
         for i in range(-1,amount):
@@ -40,13 +46,13 @@ def assign(file_input, file_output):
                 else:
                     begin               = depot if i == -1 else packages[i]
                     end                 = packages[j]
-                    weightMatrix[i][j]  = calProfit(begin,end)
+                    weightMatrix[i][j]  = CalProfit(begin,end)
 
-    def getProfit(pac1,pac2):
+    def GetProfit(pac1,pac2):
         nonlocal depot ,amount,shipperNum ,packages, weightMatrix
         return weightMatrix[pac1][pac2]
 
-    def fakeFitness(costs):
+    def Fitness(costs):
         nonlocal shipperNum
         temp    = costs.copy()
         temp.sort()
@@ -77,22 +83,22 @@ def assign(file_input, file_output):
             res = lst[lst.index(k)-1]
         return res
 
-    def part3ADN(part1,part2):
+    def Part3ADN(part1,part2):
         nonlocal amount,shipperNum
         
         part3   = []
-        profit  = getProfit(-1,part1[0])
+        profit  = GetProfit(-1,part1[0])
 
         for i in range(1,amount):
             if i in part2:
                 part3   += [profit]
-                profit   = getProfit(-1,part1[i])
+                profit   = GetProfit(-1,part1[i])
             else:
-                profit  += getProfit(part1[i-1],part1[i])
+                profit  += GetProfit(part1[i-1],part1[i])
 
         return part3 + [profit]
 
-    def initPop(n):
+    def InitPop(n):
         nonlocal amount,shipperNum
         template    = [*range(amount)]
         res         = []
@@ -114,7 +120,7 @@ def assign(file_input, file_output):
                 lst.remove(package)
                 shipper     +=[package]
 
-            res += [shipper + breaks + part3ADN(shipper,breaks)]
+            res += [shipper + breaks + Part3ADN(shipper,breaks)]
         return res 
 
     def Selection(candidatesFitnesses):
@@ -133,47 +139,51 @@ def assign(file_input, file_output):
     
     def Crossover(shipperA, shipperB, forward):
         nonlocal shipperNum,amount
-        pA      = shipperA[:amount]
-        pB      = shipperB[:amount]
-        breaks  = shipperA[amount:amount + shipperNum-1] if choice([True,False]) else shipperB[amount:amount + shipperNum-1]
-        k       = randint(0, amount - 1)
-        result  = [k]
-        resCost = []
-        cost    = getProfit(-1,k)
+        pA              = shipperA[:amount]
+        pB              = shipperB[:amount]
+        breaks          = shipperA[amount:amount + shipperNum-1] \
+            if choice([True,False]) else shipperB[amount:amount + shipperNum-1]
+
+        pCurr           = randint(0, amount - 1)
+        resPackages     = [pCurr]
+        resSalemanProf  = []
+        salemanProfit   = GetProfit(-1,pCurr)
 
         while len(pA) > 1:
             if forward:
-                x = Latter(pA,k)
-                y = Latter(pB,k)
+                pANext = Latter(pA,pCurr)
+                pBNext = Latter(pB,pCurr)
             else:
-                x = Former(pA,k)
-                y = Former(pB,k)
+                pANext = Former(pA,pCurr)
+                pBNext = Former(pB,pCurr)
 
-            pA.remove(k)
-            pB.remove(k)
+            pA.remove(pCurr)
+            pB.remove(pCurr)
 
-            dx = getProfit(k,x)
-            dy = getProfit(k,y)
+            profitPANext    = GetProfit(pCurr,pANext)
+            profitPBNext    = GetProfit(pCurr,pBNext)
 
-            temp = 0
-            if dx < dy:
-                k = x
-                temp = dx
+            profitPNext     = 0
+
+            if profitPANext < profitPBNext:
+                pCurr       = pANext
+                profitPNext = profitPANext
             else:
-                k = y
-                temp = dy
-            if len(result) in breaks:
-                resCost += [cost]
-                cost = getProfit(-1,k)
+                pCurr       = pBNext
+                profitPNext = profitPBNext
+            if len(resPackages) in breaks:
+                resSalemanProf += [salemanProfit]
+                salemanProfit = GetProfit(-1,pCurr)
             else:
-                cost+=temp
-            result.append(k)
-        resCost += [cost]
-        # print(result + breaks + resCost)
-        return result + breaks + resCost
+                salemanProfit+=profitPNext
+            resPackages.append(pCurr)
+
+        resSalemanProf += [salemanProfit]
+        return resPackages + breaks + resSalemanProf
 
     def Mutation(ADN):
         nonlocal amount,shipperNum
+
         packagelst      = ADN[:amount]
         idxBreaklst     = ADN[amount:amount + shipperNum -1]
         # print(idxBreaklst)
@@ -186,7 +196,9 @@ def assign(file_input, file_output):
                 if (idxBegin < idxEnd):
                     break
 
-            newPackagelst   = packagelst[:idxBegin] + (packagelst[idxBegin:(idxEnd+1)])[::-1] + packagelst[(idxEnd+1):]
+            newPackagelst   = packagelst[:idxBegin] + \
+                (packagelst[idxBegin:(idxEnd+1)])[::-1] + packagelst[(idxEnd+1):]
+
         else:
 
             #   method 2
@@ -196,10 +208,6 @@ def assign(file_input, file_output):
 
                 if (idx1 > 0) and (idx1 < idx2):
                     break
-
-            # part1 = packagelst[:idx1]
-            # part2 = packagelst[idx1:(idx2+1)]
-            # part3 = packagelst[(idx2+1):]
             
             newPackagelst = packagelst[idx1:(idx2+1)] + packagelst[:idx1] + packagelst[(idx2+1):]
 
@@ -211,9 +219,9 @@ def assign(file_input, file_output):
                     newIdxBreaklst.append(newidx)
                     break
             newIdxBreaklst.sort()
-        return newPackagelst + newIdxBreaklst + part3ADN(newPackagelst,newIdxBreaklst)
+        return newPackagelst + newIdxBreaklst + Part3ADN(newPackagelst,newIdxBreaklst)
 
-    def writeOutput(ADN):
+    def WriteOutput(ADN):
         nonlocal amount,shipperNum
         part1 = ADN[:amount]
         part2 = ADN[amount:shipperNum + amount - 1]
@@ -229,45 +237,53 @@ def assign(file_input, file_output):
         with open(file_output,"w") as file:
             file.write("\n".join([" ".join([str(j) for j in i]) for i in res]))
                   
-    def mainAlgo():
+    def MainAlgo():
         nonlocal amount,shipperNum
         N                       = 15
-        population              = initPop(N)
+        population              = InitPop(N)
         mutationChance          = 0.15
-        Best,BestFitness,theOne = population[0],fakeFitness(population[0][amount+shipperNum -1 :]),False
+        Best,BestFitness,theOne = population[0],Fitness(population[0][amount+shipperNum -1 :]),False
         C                       = 10000
         while C > 0 :
             C -= 1
             if theOne: break
             populationFitness = []
             for candidate in population:
-                temp = fakeFitness(candidate[amount+shipperNum -1 :])
+                temp = Fitness(candidate[amount+shipperNum -1 :])
                 if temp < BestFitness:
                     C           += 500
                     Best         = candidate
                     BestFitness  = temp
 
                     if temp - 0 < 1e-7:
-                        theOne = True
+                        theOne  = True
+
                 populationFitness += [temp]
+
             nextGen = []
             for i in range(N):
                 parentA = population[Selection(populationFitness)]
-                parentB =  population[Selection(populationFitness)]
-                child = Crossover(parentA,parentB,choice([True,False]))
+                parentB = population[Selection(populationFitness)]
+                child   = Crossover(parentA,parentB,choice([True,False]))
+
                 if random() < mutationChance:
                     child = Mutation(child)
+
                 nextGen += [child]
+
             population = nextGen
         return Best,BestFitness
     
     start = time()
-    readInput()
-    mapNode()
-    temp = mainAlgo()
-    print(temp[1])
-    writeOutput(temp[0])
+    ReadInput()
+    MapNode()
+    result = MainAlgo()
+    print(result[1])
+    WriteOutput(result[0])
     print(f"runtime {time() -start}")
 
-assign("input.txt","output.txt")
-# [5, 8, 3, 4, 1, 7, 0, 6, 2, 1, 7
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        raise "USAGE: python3 assignment2.py <input.txt> <output.txt>"
+
+    assign(sys.argv[1],sys.argv[2])
